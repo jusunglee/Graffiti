9ab55fa94e78eb134c7fc44fbde5d79eff7bf2a4
Jusung Lee <e.jusunglee@gmail.com>
2017-10-28 14:21:08 -0500

    mongo.py

---
+++ mongo.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask import render_template
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+@app.route('/')
+def home_page():
+    online_users = mongo.db.users.find({'online': True})
+    return render_template('index.html',
+        online_users=online_users)
