44bb72f0b428dc95bd389e5a416f1861308835c3
Jusung Lee <e.jusunglee@gmail.com>
2017-10-28 17:34:30 -0500

    db

--- mongo.py
+++ mongo.py
@@ -1,12 +1,24 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-from flask import render_template
+"""database contains the interface to interact directly with the firebase backend"""
+import pyrebase
 
-app = Flask(__name__)
-mongo = PyMongo(app)
 
-@app.route('/')
-def home_page():
-    online_users = mongo.db.users.find({'online': True})
-    return render_template('index.html',
-        online_users=online_users)
+def firebase():
+    """fireBase returns a firebase instance to be used when posting/reading from the database"""
+    keys = "api_auth.txt"
+    with open(keys) as file_:
+        content = file_.readlines()
+
+    config = dict()
+
+    for line in content:
+        tokens = line.split('=')
+        tokens = [token.strip() for token in tokens]
+        config[tokens[0]] = tokens[1]
+    return pyrebase.initialize_app(config)
+
+
+def push(db1, user, key, val):
+    """push takes a datbase reference a key and val as strings and pushes them
+     to the given database. No return value"""
+    db1.child(key).set(val, user['idToken'])
+    usercount = db1.child("UserCount").get(user['idToken']).val()
