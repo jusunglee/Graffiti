39a157ce8a38445f691e0cd6506b3a35d70257cd
Muin Momin <momin.muin@gmail.com>
2017-10-28 23:15:54 -0500

    starting post request

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 
-from flask import Flask, abort, make_response, jsonify
+from flask import Flask, abort, make_response, jsonify, request
 import mongo
 
 app = Flask(__name__)
@@ -9,10 +9,19 @@ app = Flask(__name__)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/<firstname>')
-def index(firstname):
-    print(mongo.push())
-    return "Pushed."
+
+@app.route('/addcomment', methods = ['POST'])
+def addComment():
+    if request.method == 'POST':
+        url = request.form['url']
+        comment = request.form['comment']
+        location = request.form['location']
+        print(url, comment, location)
+        return add_new_comment_to_db(url, comment, location)
+    else:
+        return "Bad request."
+
+
 
 if __name__ == '__main__':
     app.run(debug=False)
