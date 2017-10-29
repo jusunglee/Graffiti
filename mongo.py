110c215b212ad70bf01e18c4509d4ba9df3ddf9f
Jusung Lee <e.jusunglee@gmail.com>
2017-10-28 21:59:07 -0500

    rockstar

--- mongo.py
+++ mongo.py
@@ -17,26 +17,35 @@ def firebase():
     return pyrebase.initialize_app(config)
 
 
-def push():
-    auth()
+def push(encoded_url, comment):
     # usercount = db.child("howdy").get(user['idToken']).val()
     data = {
-        'dawins': 'dessert'
+        'comments': [
+            {
+                'comment': comment,
+                'stars': 1
+            }
+        ]
     }
-    db.child("howdy").push(data, user['idToken'])
-    # db.child("howdy").child("1").set(data)
+    encoded_url = encoded_url.replace('{{DOT}}', '.')
+    db = auth()
+    db.child("websites/").child(encoded_url).set(data)
     return "Hello"
+
 
 def auth():
     fb = firebase()
     auth = fb.auth()
     user = auth.sign_in_with_email_and_password("test@test.com", "ganggang")
     db = fb.database()
-    return 
+    return db
+
 
 def top10Comments(url):
-    auth()
+    db = auth()
     comments = db[url]
     print(comments)
     # comments.sort() # thumbs up are the first element in the tuple
     # return comments[:10]
+
+push('https%3A%2F%2Fwww.nytimes.com%2F2017%2F08%2F12%2Fus%2Fpolitics%2Felizabeth-warren-democrats-liberals{{DOT}}html%3F_r%3D1','im gonna knock it. it\'s a little gay')
