648f595b7c10886974909bf76dc30850c1ab7c7a
Muin Momin <momin.muin@gmail.com>
2017-10-29 00:08:32 -0500

    back end compelte

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def not_found(error):
 def addComment():
     if request.method == 'POST':
         d = dict(request.form)
-        print(d)
         url = d['url'][0]
         comment = d['comment'][0]
         location = d['location'][0]
         return mongo.add_new_comment_to_db(url, comment, location)
     else:
         return "Bad request."
+
 
 @app.route('/gettopcomments', methods = ['GET'])
 def getTopComments():
@@ -29,8 +29,21 @@ def getTopComments():
         k = int(request.args.get('k'))
         print(url, k)
         top_comments = mongo.get_top_k_comments(url, k)
-        d = {'top_comments': top_comments}
-        return jsonify(d)
+        return jsonify(top_comments)
+    else:
+        return "Bad request."
+
+
+@app.route('/addlike', methods = ['POST'])
+def addLike():
+    if request.method == 'POST':
+        d = dict(request.form)
+        url = d['url'][0]
+        comment_hash = d['comment_hash'][0]
+        return mongo.push_comment_like_to_db(url, comment_hash)
+    else:
+        return "Bad request."
+
 
 if __name__ == '__main__':
     app.run(debug=False)
